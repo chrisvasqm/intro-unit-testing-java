b938489dc22d275710ebdec7e225be6fe52e79bd
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-04-24 18:13:19 -0400

    Add divide tests

--- src/test/java/MathTests.java
+++ src/test/java/MathTests.java
@@ -5,12 +5,26 @@ import static org.testng.Assert.assertEquals;
 public class MathTests {
 
     @Test
-    public void sum_TwoPlusTwo_ReturnsFour() {
+    public void add_TwoPlusTwo_ReturnsFour() {
         final int expected = 4;
 
         final int actual = Math.add(2, 2);
 
         assertEquals(actual, expected);
+    }
+
+    @Test
+    public void divide_TenDividedByFive_ReturnsTwo() {
+        final double expected = 2.0;
+
+        final double actual = Math.divide(10, 5);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void divide_TenDividedByZero_ThrowsIllegalArgumentException() {
+        Math.divide(10, 0);
     }
 
 }
